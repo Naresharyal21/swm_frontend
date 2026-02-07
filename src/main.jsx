ee279853f74ff1e8c21812ba849698ed8a91cc3f
Naresh aryal <naresh.211421@ncit.edu.np>
2026-02-07 21:35:12 +0545

    theme updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import "leaflet/dist/leaflet.css";
-
-
-
+import 'leaflet/dist/leaflet.css'
 
 import './styles/index.css'
 import { ThemeProvider } from './providers/ThemeProvider'
 import { QueryProvider } from './providers/QueryProvider'
 import { AuthProvider } from './providers/AuthProvider'
 import App from './App'
+
+// ✅ Prevent transition flash on first paint
+document.documentElement.classList.add('preload')
+requestAnimationFrame(() => {
+  document.documentElement.classList.remove('preload')
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
